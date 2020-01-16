b72791f44b27be95e03b20e6ddd4f6ab0225c131
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-16 10:59:42 -0600

    Used intersection, union and discard

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ showroom.add("Mustang")
 showroom.add("Model 3")
 showroom.add("Impala")
 showroom.add("Leaf")
-print(showroom)
+# print(showroom)
 
 """
 Print the length of your set.
 """
 
-print(len(showroom))
+# print(len(showroom))
 
 """
 Pick one of the items in your show room and add it to the set again.
@@ -30,7 +30,7 @@ showroom.add("Mustang")
 Print your showroom. Notice how there's still only one instance of that model in there.
 """
 
-print(showroom)
+# print(showroom)
 
 """
 Using update(), add two more car models to your showroom with another set.
@@ -38,7 +38,7 @@ Using update(), add two more car models to your showroom with another set.
 
 showroom.update({"Batmobile", "Mystery Machine"})
 
-print(showroom)
+# print(showroom)
 
 """
 You've sold one of your cars. Remove it from the set with the discard() method.
@@ -46,4 +46,44 @@ You've sold one of your cars. Remove it from the set with the discard() method.
 
 showroom.discard("Leaf")
 
-print(showroom)
+# print(showroom)
+
+"""
+Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+"""
+
+junkyard = set()
+
+junkyard.add("Mustang")
+junkyard.add("Model 3")
+junkyard.add("Lumina")
+junkyard.add("PT Cruiser")
+junkyard.add("Cobra")
+junkyard.add("Party Wagon")
+junkyard.add("DeLorean")
+
+# print(junkyard)
+
+"""
+Use the intersection method to see which cars exist in both the showroom and that junkyard.
+"""
+
+match = showroom.intersection(junkyard)
+
+# print("Matches:", match )
+
+"""
+Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+"""
+
+master_list = showroom.union(junkyard)
+
+# print("Master List:", master_list)
+
+"""
+Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
+"""
+
+junkyard.discard("PT Cruiser")
+
+print("Less Junky:", junkyard)
