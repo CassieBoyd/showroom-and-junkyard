081dd4628d42114979ba4b476d4bb5ee68ebb151
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-15 20:42:46 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Create an empty set named showroom.
+"""
+# Note: to create an empty set you have to use set(), not {}; the latter creates an empty dictionary
+
+showroom = set()
+
+"""
+Add four of your favorite car model names to the set.
+"""
+
+showroom.add("Mustang")
+showroom.add("Model 3")
+showroom.add("Impala")
+showroom.add("Leaf")
+print(showroom)
+
+"""
+Print the length of your set.
+"""
+
+print(len(showroom))
